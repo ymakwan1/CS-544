7c9105330f4727f03c77743d2fde329798e99888
Yash Makwana <ymakwan1@binghamton.edu>
2023-03-09 23:11:17 -0500

    variable name change

--- submit/prj2-sol/src/lib/grades-dao.ts
+++ submit/prj2-sol/src/lib/grades-dao.ts
@@ -74,23 +74,35 @@ export class GradesDao {
     //return null;
   }
   
+  /**
+   * This is updating the grades collection with the new rawTable
+   * @param {string} courseId - The courseId of the course that the grades are being updated for.
+   * @param rawTable - This is the raw table that is being passed in.
+   * @returns The grades are being returned.
+   */
   async #write(courseId:string, rawTable : G.RawTable): Promise<Result<G.Grades>>{
 
+/* This is updating the grades collection with the new rawTable. */
     try {
       const collection = this.#grades;
       
       const filter = {courseId};
-      const update = {$set : {[courseId]: courseId, rawTable}};
-      //const update = {$set : {rawTable}};
+      //const update = {$set : {[courseId]: courseId, rawTable}};
+      const update = {$set : {rawTable}};
       const options = {returnDocument: mongo.ReturnDocument.AFTER, upsert : true};
 
       const updateResult =  await collection.findOneAndUpdate(filter, update, options);
-      //this.close()
+      
+      /* This is checking if the update was successful or not. If the update was successful, it will
+      return the grades. If the update was not successful, it will return an error. */
       if(!updateResult){
-        return errResult(`Wrong update`, 'DB');
-      } else if (!updateResult.value) {
+        return errResult(`Wrong update for courseId : ${courseId}`, 'DB');
+      } 
+      else if (!updateResult.value) {
         return errResult(`No grades found for courseId : ${courseId}`, 'NOT_FOUND');
-      } else{
+      } 
+      else{
+      /* This is taking the rawTable from the updateResult and returning the grades. */
         const grades = {...updateResult.value.rawTable};
         return GradesImpl.makeGradesWithData(courseId, Object.values(grades));
       }
@@ -99,11 +111,19 @@ export class GradesDao {
     }
   }
 
+  /**
+   * It reads the grades for a course from the database
+   * @param {string} courseId - string
+   * @returns A promise that resolves to a result object.
+   */
   async #read(courseId : string){
+    /* This is checking if the courseId is valid or not. If the courseId is not valid, it will return
+    an error. */
     const courseCheck = checkCourseId(courseId);
     if (!courseCheck.isOk) {
       return errResult(`unknown course id ${courseId}`, 'BAD_ARG');
     }
+    /* This is reading the grades for a course from the database. */
     try {
       const collection = this.#grades;
       const gradeEntry = await collection.findOne({courseId});
@@ -124,18 +144,20 @@ export class GradesDao {
    *   BAD_ARG: courseId is not a valid course-id.
    */
   async getGrades(courseId: string): Promise<Result<G.Grades>> {
+    /* This is checking if the courseId is valid or not. If the courseId is not valid, it will return
+        an error. */
     const courseCheck = checkCourseId(courseId);
     if (!courseCheck.isOk) {
       return errResult(`unknown course id ${courseId}`, 'BAD_ARG');
     }
-    const ab = this.#read(courseId);
-    return ab;
+/* This is reading the grades for a course from the database. */
+    const getGradesRead = this.#read(courseId);
+    return getGradesRead;
   }
 
   /** Remove all course grades stored by this DAO */
   async clear() : Promise<Result<void>> {
-    //TODO
-    //return null;
+    /* This is deleting all the grades from the database. */
     try {
       await this.#grades.deleteMany({});
       return okResult(undefined);
@@ -165,21 +187,25 @@ export class GradesDao {
   async upsertRows(courseId: string, rows: G.RawRow[])
     : Promise<Result<G.Grades>> 
   {
-    //TODO
+    /* This is checking if the courseId is valid or not. If the courseId is not valid, it will return an error. */
     const courseCheck = checkCourseId(courseId);
     if(!courseCheck.isOk){
       return errResult(`unknown course id ${courseId}`, 'BAD_ARG');
     }
 
-    const readResults = await this.getGrades(courseId);
-    if (readResults.isOk) {
-      let readData = [...readResults.val.getRawTable(), ...rows];
-      const ab = readResults.val.upsertRows(readData);
-      if (ab.isOk) {
-        await this.#write(courseId, ab.val.getRawTable());
+    /* This is reading the grades for a course from the database. */
+    const upsertRowsReadResults = await this.getGrades(courseId);
+
+    /* This is checking if the upsertRowsReadResults is ok or not. If the upsertRowsReadResults is ok,
+    it will upsert the rows to the grades. If the upsertRowsResults is ok, it will write the grades to the
+    database. */
+    if (upsertRowsReadResults.isOk) {
+      let readData = [...upsertRowsReadResults.val.getRawTable(), ...rows];
+      const upsertRowsResults = upsertRowsReadResults.val.upsertRows(readData);
+      if (upsertRowsResults.isOk) {
+        await this.#write(courseId, upsertRowsResults.val.getRawTable());
       } else {
-        //return errResult(`unknown course id ${courseId}`, 'RANGE');
-        return ab;
+        return upsertRowsResults;
       }
     }
     return okResult(undefined);
@@ -202,19 +228,23 @@ export class GradesDao {
   async addColumns(courseId: string, ...colIds: string[])
     : Promise<Result<G.Grades>>
   {
+    /* This is checking if the courseId is valid or not. If the courseId is not valid, it will return an error. */
     const courseCheck = checkCourseId(courseId);
     if(!courseCheck.isOk){
       return errResult(`unknown course id ${courseId}`, 'BAD_ARG');
     }
 
-    const readData = await this.getGrades(courseId);
+    /* This is reading the grades for a course from the database. */
+    const addColumnsReadResults = await this.getGrades(courseId);
 
-    if(readData.isOk){
-      const ab = readData.val.addColumns(...colIds);
-      //console.log(ab)
+    /* This is checking if the addColumnsReadResults is ok or not. If the addColumnsReadResults is ok,
+    it will add the columns to the grades. If the addColumnsResults is ok, it will write the grades
+    to the database. */
+    if(addColumnsReadResults.isOk){
+      const addColumnsResults = addColumnsReadResults.val.addColumns(...colIds);
 
-      if (ab.isOk) {
-        await this.#write(courseId, ab.val.getRawTable())
+      if (addColumnsResults.isOk) {
+        await this.#write(courseId, addColumnsResults.val.getRawTable())
       }
     }
     
@@ -229,21 +259,21 @@ export class GradesDao {
   async patch(courseId: string, patches: G.Patches)
     : Promise<Result<G.Grades>> 
   { 
-    //TODO
-    //return null;
+    /* This is checking if the courseId is valid or not. If the courseId is not valid, it will return an error. */
     const courseCheck = checkCourseId(courseId);
     if(!courseCheck.isOk){
       return errResult(`unknown course id ${courseId}`, 'BAD_ARG');
     }
 
-    const readData = await this.getGrades(courseId);
+    /* This is reading the grades for a course from the database. */
+    const patchReadDataResults = await this.getGrades(courseId);
 
-    if(readData.isOk){
-      const ab = readData.val.patch(patches);
-      //console.log(ab)
-
-      if (ab.isOk) {
-        await this.#write(courseId, ab.val.getRawTable())
+    /* This is checking if the patchReadDataResults is ok or not. If the patchReadDataResults is ok, it
+    will patch the grades. If the patchResults is ok, it will write the grades to the database. */
+    if(patchReadDataResults.isOk){
+      const patchResults = patchReadDataResults.val.patch(patches);
+      if (patchResults.isOk) {
+        await this.#write(courseId, patchResults.val.getRawTable())
       }
     }
     
