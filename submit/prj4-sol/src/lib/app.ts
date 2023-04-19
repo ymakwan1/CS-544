783ab4b476223d6f7d2fb0193557692b1e0278b9
Yash Makwana <ymakwan1@binghamton.edu>
2023-04-18 20:38:41 -0400

    intiali table done

--- submit/prj4-sol/src/lib/app.ts
+++ submit/prj4-sol/src/lib/app.ts
@@ -1,7 +1,7 @@
 import { CourseInfo as C, GradeTable as G, GradesImpl, COURSES }
   from 'cs544-prj1-sol';
 
-import { Err, Result, okResult, errResult, ErrResult } from 'cs544-js-utils';
+import { Err, Result, okResult, errResult, ErrResult, OkResult } from 'cs544-js-utils';
 
 /** factory function to create an App and take care of any
  *  asynchronous initialization.
@@ -41,52 +41,140 @@ class WebServices {
 }
 
 class App {
-
+  private wsUrl : string;
   private courseIdSelect: HTMLSelectElement;
   private studentIdSelect : HTMLInputElement;
   private showStatsCheckbox : HTMLInputElement;
   private gradesForm : HTMLFormElement; 
   private webService : WebServices
+  private table : HTMLElement
   constructor(wsUrl: string) {
     //TODO
     //cache form HTMLElements as instance vars, set up handlers, web services
 
-    this.webService = new WebServices(wsUrl);
+    //this.webService = new WebServices(wsUrl);
+
+    this.wsUrl = wsUrl;
 
     this.courseIdSelect = document.querySelector('#course-id') as HTMLSelectElement;
     this.courseIdSelect.append(...coursesOptions())
 
-    
-
-    this.gradesForm = document.querySelector('#grades-form') as HTMLFormElement;
-    this.gradesForm.addEventListener('submit', (ev: Event) => {
-      ev.preventDefault(); 
-    });
-    
-
-    const changeHandler = (ev: Event) => {
-      //ev.preventDefault()
-      console.log(ev);
-    };
     this.studentIdSelect = document.querySelector('#student-id') as HTMLInputElement;
     this.showStatsCheckbox = document.querySelector('#show-stats') as HTMLInputElement;
+    this.gradesForm = document.querySelector('#grades-form') as HTMLFormElement;
+    this.table = document.querySelector('#grades') as HTMLElement;
+
     this.courseIdSelect.addEventListener('change', this.handlerChange)
-    this.courseIdSelect.addEventListener('change', this.handlerChange);
+    this.showStatsCheckbox.addEventListener('change', this.handlerChange);
     this.studentIdSelect.addEventListener('change', this.handlerChange);
+    //this.gradesForm.addEventListener('submit', this.changeHandler);
+    this.gradesForm.addEventListener('change', (ev:Event) => this.changeHandler(ev));
+    
+    const r = this.getCourseGrades('cs220').then((response:OkResult<any>|ErrResult)=>{
+      if (response.isOk) {
+        console.log(response);
+        this.generateTable(response.val.result);
+      }
+      
+    });
+    // console.log("From const" + );
+    // changeHandler(ev:Event) {
+    //   ev.preventDefault()
+    //   this.initialize();
+    //   console.log(getFormData(this.gradesForm.form))
+    //   console.log(ev);
+    // };
+    
 
-    this.gradesForm.addEventListener('change', changeHandler);
+    //this.gradesForm.addEventListener('change', changeHandler);
 
   }
-  //TODO: add methods/data as necessary.
-  async initialize(){
-    const r = await this.webService.getCourseGrades('cs220');
+  removeAllChildNodes(parent:HTMLElement) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+  }
+}
+
+  async getCourseGrades(courseId:string, rowId?:string, full?:string){
+    try {
+      let url = `${this.wsUrl}/grades/${courseId}`;
+      if (rowId && rowId !== '' ) {
+      url += `/${rowId}`;
+      }
+      if (full) {
+      url += '?full=true';
+      }
+      const response = await fetch(url)
+      if(response.ok){
+        const data  =  await response.json();
+        //console.log(data);
+        if (!data.isOk) {
+          return errResult(data);
+        } else {
+          const d = GradesImpl.makeGradesWithData(courseId, data.result);
+          //console.log(d);
+          return okResult(data);
+        }
+      } else {
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (error) {
+      return errResult;
+    }
+  }
+
+
+
+  changeHandler = async(ev:Event) =>{
+    ev.preventDefault()
+    const r = await this.getCourseGrades('cs220');
+    // if (r.) {
+      
+    // }
     console.log(r);
+    const formData = getFormData(this.gradesForm);
+    const cs544 = await this.getCourseGrades(formData.courseId, formData.rowId);
+    
+    console.log(cs544);
+    console.log(formData);
+    console.log(ev);
+  };
+  //TODO: add methods/data as necessary.
+
+  handlerChange(ev : Event){
+    console.log(ev)
   }
 
-  handlerChange(){
-    const changeHandler = (ev: Event) => {
-      console.log(ev);
-    };
+  generateTable(data:any){
+    console.log(data)
+    this.removeAllChildNodes(this.table)
+  const headerRow = makeElement("tr");
+  const headers = Object.keys(data[0])
+// Create table header columns
+  for (const header of headers) {
+    const th = makeElement("th", {}, header);
+    headerRow.appendChild(th);
+  }
+
+// Append header row to the table
+  this.table.appendChild(headerRow);
+
+// Create table data rows
+for (const row of data) {
+  console.log(row);
+  const tr = makeElement("tr");
+  for (const header of headers) {
+    console.log(row[header]);
+    const val=row[header].toString();
+    const td = makeElement("td", {}, val);
+    tr.append(td);
+  }
+  this.table.append(tr);
+}
+
+// Append the table to the DOM
+document.body.appendChild(this.table);
   }
 }
 
