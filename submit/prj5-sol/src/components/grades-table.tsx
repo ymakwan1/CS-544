6c6d176f5fd9c431a47633795eca5f7e8b0afb28
Yash Makwana <ymakwan1@binghamton.edu>
2023-05-03 08:00:41 -0400

    final

--- submit/prj5-sol/src/components/grades-table.tsx
+++ submit/prj5-sol/src/components/grades-table.tsx
@@ -120,29 +120,29 @@
 // //   ));
 // //   return <tr>{cells}</tr>;
 // // }
-// function DataRow(props: DataRowProps) {
-//   const { dataRow, courseInfo, changeGrade } = props;
-//   const roundVal = (val: number) => val.toFixed(1);
-//   const cells = Object.entries(dataRow).map(([colId, val]) => {
-//     const isEditable = dataRow[courseInfo.rowIdColId] !== '' && courseInfo.cols[colId]?.kind === 'score';
-//       return (
-//         <td key={colId}>
-//           {isEditable ? (
-//             <GradeInput
-//               rowId={dataRow[courseInfo.rowIdColId].toString()}
-//               colId={colId}
-//               val={String(val)}
-//               changeGrade={changeGrade}
-//             />
-//           ) : typeof val === "number" ? (
-//             roundVal(val)
-//           ) : (typeof val === 'object' ? JSON.stringify(val) : val)
-//           }
-//         </td>
-//       );
-//   });
-//   return <tr>{cells}</tr>;
-// }
+function DataRow(props: DataRowProps) {
+  const { dataRow, courseInfo, changeGrade } = props;
+  const roundVal = (val: number) => val.toFixed(1);
+  const cells = Object.entries(dataRow).map(([colId, val]) => {
+    const isEditable = dataRow[courseInfo.rowIdColId] !== '' && courseInfo.cols[colId]?.kind === 'score';
+      return (
+        <td key={colId}>
+          {isEditable ? (
+            <GradeInput
+              rowId={dataRow[courseInfo.rowIdColId].toString()}
+              colId={colId}
+              val={String(val)}
+              changeGrade={changeGrade}
+            />
+          ) : typeof val === "number" ? (
+            roundVal(val)
+          ) : (typeof val === 'object' ? JSON.stringify(val) : val)
+          }
+        </td>
+      );
+  });
+  return <tr>{cells}</tr>;
+}
 
 
 // type GradeInputProps = {
@@ -175,7 +175,7 @@
 //   );
 // }
 
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { GradesWs } from '../lib/grades-ws.js';
 
@@ -195,8 +195,6 @@ type GradesTableProps = {
 export default function GradesTable(props: GradesTableProps) {
   const { ws, courseId, courseInfo, grades, setResult } = props;
   const dataRows = grades.getFullTable();
-  //const dataRows : any = [];
- // console.log(dataRows);
   if (!dataRows.length) {
     return (
       <table>
@@ -204,20 +202,16 @@ export default function GradesTable(props: GradesTableProps) {
       </table>
     );
   }
-  //console.log(props.courseId);
   const hdrs = Object.keys(dataRows[0]);
   const changeGrade = async (rowId: string, colId: string, val: string) => {
     const numVal = parseFloat(val);
-    if (!isNaN(numVal) || val === '') {
+    if (!isNaN(numVal) && !/[a-zA-Z]/.test(val)) {
       const patches:G.Patches = { [rowId]: { [colId]: numVal } };
-    const result = await ws.updateCourseGrades(courseId, patches);
-    console.log(result);
-    if(result.isOk){
+      const result = await ws.updateCourseGrades(courseId, patches);
+      //console.log(result)
       setResult(result);
     } else {
-      console.log(result.errors);
-      return setResult(result);
-    }
+      setResult(errResult(`value ${val} for ${colId} contains a letter`));
     }
   };
   return (
@@ -260,7 +254,6 @@ type HeaderProps = {
 };
 
 function Header(props: HeaderProps) {
-  //console.log(props.hdrs);
   return (
     <tr>
       {props.hdrs.map((hdr) => (
@@ -293,34 +286,6 @@ type DataRowProps = {
   changeGrade: (rowId: string, colId: string, val: string) => void,
 };
 
-function DataRow(props: DataRowProps) {
-  const {dataRow, courseInfo, changeGrade} = props;
-  return(
-    <tr>
-      {Object.entries(dataRow).map(([colId, val]) => {
-        //console.log(courseInfo.cols[colId]?.kind === 'score');
-        if (courseInfo.id === 'cs544' && typeof val === 'number') {
-          val = val.toFixed(1);
-        }
-        if (props.courseInfo.cols[colId]?.kind === 'score' && dataRow[courseInfo.rowIdColId] !== '') {
-          return (
-            <td key={colId}>
-            {/* <GradeInput rowId={dataRow[courseInfo.rowIdColId]} colId={colId} val={val} changeGrade={changeGrade} /> */}
-            <GradeInput rowId={dataRow[courseInfo.rowIdColId].toString()} colId={colId} val={String(val)} changeGrade={changeGrade} />
-            </td>
-            );
-        } else {
-        return (
-          <td key={colId}>
-            {typeof val === 'object' ? JSON.stringify(val) : val}
-          </td>
-        );}
-      })}
-    </tr>
-  );
-}
-
-
 type GradeInputProps = {
   rowId: string,
   colId: string,
@@ -328,37 +293,21 @@ type GradeInputProps = {
   changeGrade: (rowId: string, colId: string, val: string) => void,
 };
 
-// function GradeInput(props: GradeInputProps) {
-//   const { rowId, colId, val, changeGrade } = props;
-//   return <></>;
-// }
-// function GradeInput(props: GradeInputProps) {
-//   const { rowId, colId, val, changeGrade } = props;
-//   const [value, setValue] = React.useState(val);
-//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setValue(event.target.value);
-//   };
-//   const handleBlur = async () => {
-//     if (value !== val) {
-//       await changeGrade(rowId, colId, value);
-//     }
-//   };
-//   return (
-//     <input type="text" value={value} onChange={handleChange} onBlur={handleBlur} />
-//   );
-// }
 function GradeInput(props: GradeInputProps) {
   const { rowId, colId, val, changeGrade } = props;
   const [value, setValue] = React.useState(val);
 
+  const num = useRef(val);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
     setValue(e.target.value);
-    //changeGrade(rowId, colId, value);
   };
 
-  const handleBlur = () => {
-    if (value !== val) {
-      changeGrade(rowId, colId, value);
+  const handleBlur = async () => {
+    if (num.current !== value) {
+      //console.log(value, val)
+      num.current=value;
+      await changeGrade(rowId, colId, value);
     }
   };
 
@@ -372,30 +321,3 @@ function GradeInput(props: GradeInputProps) {
     />
   );
 }
-
-// function GradeInput(props: GradeInputProps) {
-//   const { rowId, colId, val, changeGrade } = props;
-//   const [value, setValue] = React.useState(val);
-  
-//   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     // ignore any input typed into the input widget
-//     event.preventDefault();
-//   }
-
-//   const handleBlur = async () => {
-//     if (val !== value) {
-//       await changeGrade(rowId, colId, value);
-//     }
-//   };
-
-//   return (
-//     <input
-//       type="text"
-//       value={value}
-//       size={3}
-//       onChange={(event) => setValue(event.target.value)}
-//       onBlur={handleBlur}
-//       onInput={handleInput}
-//     />
-//   );
-// }
